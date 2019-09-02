58981a7039987d43d0da3d5cba9fe76a66c5c03b
jgchenu <jgchenu@foxmail.com>
2019-09-02 18:18:04 +0800

    feat: reselect 函数优化

---
+++ src/todos/selector.js
@@ -0,0 +1,22 @@
+import { createSelector } from  'reselect';
+import * as filterTypes from '@/filter/constants';
+
+
+const getFilter = state => state.filter;
+const getTodos = state => state.todos; 
+
+export const selectVisibleTodos = createSelector(
+  [getFilter, getTodos],
+  (filter, todos) => {
+    switch(filter){
+      case filterTypes.ALL:
+        return todos;
+      case filterTypes.COMPLETED:
+        return todos.filter(item=>item.completed);
+      case filterTypes.UNCOMPLETED:
+        return todos.filter(item=>!item.completed);
+      default:
+        throw Error('filter is not the expecting type');
+    }
+  }
+)
--- src/todos/views/todoList/container.js
+++ src/todos/views/todoList/container.js
@@ -1,22 +1,9 @@
 import  { connect }  from 'react-redux';
-import * as filterTypes from '@/filter/constants';
 import TodoList from './TodoList';
-
-const selectVisiableTodos = (todos,filter) => {
-  switch(filter){
-    case filterTypes.ALL:
-      return todos;
-    case filterTypes.COMPLETED:
-      return todos.filter(item=>item.completed);
-    case filterTypes.UNCOMPLETED:
-      return todos.filter(item=>!item.completed);
-    default:
-      throw Error('filter is not the expecting type');
-  }
-}
+import  { selectVisibleTodos } from '@/todos/selector'
 
 const mapStateToProps = (state) => ({
-  todos: selectVisiableTodos(state.todos, state.filter)
+  todos: selectVisibleTodos(state)
 })
 
 
